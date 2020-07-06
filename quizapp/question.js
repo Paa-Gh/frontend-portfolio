8a8ad22961e6e95edfe3dd831a5e024619165401
Paa-Gh <67100385+Paa-Gh@users.noreply.github.com>
2020-07-06 03:29:31 +0000

    Uploading quiz app

---
+++ quizapp/question.js
@@ -0,0 +1,73 @@
+//Questions from: https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+
+var questions = [{
+"question": "Which of the following is correct about features of JavaScript?",
+"option1": "JavaScript is is complementary to and integrated with HTML.",
+"option2": "JavaScript is open and cross-platform.",
+"option3": "Both of the above.",
+"option4": "All of the above.",
+"answer": "3"
+},{
+"question": "Which of the following is the correct syntax to print a page using JavaScript?",
+"option1": "window.print();",
+"option2": "browser.print();",
+"option3": "navigator.print();",
+"option4": "document.print();",
+"answer": "1"
+},{
+"question": "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+"option1": "last()",
+"option2": "put()",
+"option3": "push()",
+"option4": "None of the above.",
+"answer": "3"
+},{
+"question": "Which built-in method sorts the elements of an array?",
+"option1": "changeOrder(order)",
+"option2": "order()",
+"option3": "sort()",
+"option4": "None of the above.",
+"answer": "3"
+},{ 
+"question": "Which of the following function of Number object returns a string value version of the current number in a format that may vary according to a browser's locale settings.?",
+"option1": "toExponential()",
+"option2": "toFixed()",
+"option3": "toLocaleString()",
+"option4": "toString()",
+"answer": "3"
+},{
+"question": "Which of the following function of String object returns the index within the calling String object of the last occurrence of the specified value?",
+"option1": "lastIndexOf()",
+"option2": "search()",
+"option3": "substr()",
+"option4": "indexOf()",
+"answer": "1"
+},{
+"question": "Which of the following function of String object returns the primitive value of the specified object.",
+"option1": "toLocaleUpperCase()",
+"option2": "toUpperCase()",
+"option3": "toString()",
+"option4": "valueOf()",
+"answer": "4"
+},{
+"question": "Which of the following function of String object creates an HTML hypertext link that requests another URL?",
+"option1": "link()",
+"option2": "sub()",
+"option3": "sup()",
+"option4": "small()",
+"answer": "1"
+},{
+"question": "Which of the following function of Array object creates a new array with the results of calling a provided function on every element in this array?",
+"option1": "push()",
+"option2": "join()",
+"option3": "pop()",
+"option4": "map()",
+"answer": "4"
+},{
+"question": "Which of the following function of Array object returns a string representing the array and its elements?",
+"option1": "toSource()",
+"option2": "sort()",
+"option3": "splice()",
+"option4": "toString()",
+"answer": "4"
+}];
